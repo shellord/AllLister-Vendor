d1f559ccb07b7d0e784f1aeced5c5b6278ff6dbe
saheen shoukath <saheenshoukath@gmail.com>
2021-01-25 19:29:27 +0530

    chagned files

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react'
+import { StyleSheet, Text, View, BackHandler } from 'react-native'
+import { WebView } from 'react-native-webview'
+import Constants from 'expo-constants'
 
 export default function App() {
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
+  const [currentUrl, setCurrentUrl] = useState('')
+  const webviewRef = useRef(null)
+
+  backButtonHandler = () => {
+    console.log('back')
+    if (webviewRef.current) webviewRef.current.goBack()
+    return true
+  }
+
+  frontButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goForward()
+  }
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', backButtonHandler)
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', backButtonHandler);
+
+    }
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <WebView
+      source={{
+        uri: 'http://alllisterapi.ddns.net/vendor'
+      }}
+      style={{ marginTop: Constants.statusBarHeight }}
+      renderLoading={() => (
+        <ActivityIndicator
+          color='black'
+          size='large'
+          style={styles.flexContainer}
+        />
+      )}
+      ref={webviewRef}
+      onNavigationStateChange={navState => {
+        setCanGoBack(navState.canGoBack)
+        setCanGoForward(navState.canGoForward)
+        setCurrentUrl(navState.url)
+      }}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +56,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
